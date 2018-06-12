67e6a2a9d846636b3139cc6bd8ed29559bb0230e
sofia ongele <sofiaongele@gmail.com>
2018-06-12 04:47:31 +0000

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,16 @@
-class Model
-  # Replace with Dog clas
+class Dog
+  
+  attr_accessor :name, :breed, :age
+  
+  @@dogs = []
+  
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@dogs << name
+  end
+  def self.all
+    @@dogs
+  end
 end
